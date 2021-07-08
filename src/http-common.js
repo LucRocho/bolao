ed89fe98b40dd987877337e619a828bf86954c20
Raul Cesar <raul.teixeira@gmail.com>
2021-07-08 16:59:55 -0300

    mudado para funcionar em prod

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 export default axios.create({
-  baseURL: "http://localhost:4101/api",
+  // baseURL: "http://localhost:4101/api",
+  baseURL: "/api",
   headers: {
     "Content-type": "multipart/form-data"
   }
